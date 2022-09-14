882927e470eeab5a4bbb6dca2d3876ab81b1edef
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2022-09-15 02:08:33 +0900

    今後のためにAPI用URLを追加

--- template.auth.js
+++ template.auth.js
@@ -1,7 +1,10 @@
 /// 各種パスワードなど
 
-// バックエンドサーバーのWebSocketURL（ws://...）
-const WS_URL = "<url>";
+// バックエンドサーバーのWebSocketURL（wss://...）
+const WS_URL = "<ws_url>";
+
+// バックエンドサーバーのAPIURL（https://）
+const API_URL = "<api_url>";
 
 // MapBoxのマップURL（mapbox://...）
 const MAP_URL = "<map_url>";
